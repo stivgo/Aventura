ce085299941d16a44f4ed1bb4397404e010a925d
stivgo <stiveng0630@gmail.com>
2021-09-10 15:20:51 -0500

    Create Component Recordatorio

---
+++ src/Components/Recordatorio.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Recordatorio extends Component {
+  render() {
+    return (
+      <section className="recordatorio">
+        <h3>Selección anterior: </h3>
+        <h4>Historial de opciones elegidas: </h4>
+      </section>
+    );
+  }
+}
+
+export default Recordatorio;
